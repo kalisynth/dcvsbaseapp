96fe77fa4eaaa7805619f7158207b39bdfa9b097
kalisynth <cade2002@gmail.com>
2016-03-16 10:25:02 +1000

    morning16

--- app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
+++ app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
@@ -3,7 +3,10 @@ package com.dcvs.nac.dcvsbaseapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class clientscreen extends Activity {
 
@@ -11,10 +14,19 @@ public class clientscreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientscreen);
+
+        EditText clientnametxt = (EditText)findViewById(R.id.clientNameinput);
+        clientnametxt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+    @Override
+        public boolean onEditorAction(TextView v, int clientnameId, KeyEvent event) {
+
     }
+            });
 
     public void backbtnOnClick(View v){
         startActivity(new Intent(clientscreen.this, MainActivity.class));
     }
+
+
 
 }
