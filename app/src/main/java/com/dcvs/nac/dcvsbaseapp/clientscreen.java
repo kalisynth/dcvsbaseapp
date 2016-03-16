490d7a99ce15b60896ae8cdb914776aed7050a2f
kalisynth <cade2002@gmail.com>
2016-03-16 13:17:36 +1000

    todays changes

--- app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
+++ app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
@@ -1,5 +1,7 @@
 package com.dcvs.nac.dcvsbaseapp;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
@@ -7,21 +9,41 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class clientscreen extends Activity {
+
+    public static Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientscreen);
+        Account account = getAccount(AccountManager.get(this));
+        String accountName = account.name;
+        String fullname = accountName.substring(0,accountName.lastIndexOf("@"));
+        final String tabletname = fullname;
 
-        EditText clientnametxt = (EditText)findViewById(R.id.clientNameinput);
+        EditText clientnametxt = (EditText) findViewById(R.id.clientNameinput);
         clientnametxt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-    @Override
-        public boolean onEditorAction(TextView v, int clientnameId, KeyEvent event) {
-
+            @Override
+            public boolean onEditorAction(TextView v, int clientnameId, KeyEvent event) {
+                String inputText = v.getText().toString();
+                Toast.makeText(clientscreen.this, "Hi " + inputText + " My Name is " + tabletname, Toast.LENGTH_SHORT).show();
+                ((TextView)findViewById(R.id.greetingTxt)).setText(getString(R.string.greetings_message, inputText, tabletname));
+                return true;
+            }
+        });
     }
-            });
 
     public void backbtnOnClick(View v){
         startActivity(new Intent(clientscreen.this, MainActivity.class));
