1e1dc784e0ee8a76ecb95b1d44c74f2c52f17077
kalisynth <cade2002@gmail.com>
2016-03-17 17:26:21 +1000

    added buttons to client screen

--- app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
+++ app/src/main/java/com/dcvs/nac/dcvsbaseapp/clientscreen.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
